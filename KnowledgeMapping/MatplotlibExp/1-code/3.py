6250204cfbf69f7a5c2a36bd42415b2ab3c95502
[liqian] <[liqian@adts.com]>
2018-01-10 18:25:16 +0800

    绘制采集速度的时序图...

---
+++ KnowledgeMapping/MatplotlibExp/1-code/3.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+
+
+# time作为索引
+df = pd.read_csv("./count.csv", index_col=None, parse_dates=True, names=["total"], dtype={"total": np.int64}, encoding="utf-8")
+increment = df['total'].diff()
+df["increment"] = increment
+fig, ax = plt.subplots()
+fig.set_size_inches(10, 7)
+
+
+# ax.plot_date(df.index.to_pydatetime(), df["total"], 'v-')
+# # ax.plot_date(df["time"], df["total"], 'v-')
+# ax.xaxis.set_minor_locator(dates.HourLocator())
+# ax.xaxis.set_minor_formatter(dates.DateFormatter('\n%H:%M'))
+# ax.xaxis.grid(True, which="minor")
+# ax.yaxis.grid()
+# ax.xaxis.set_major_locator(dates.DayLocator())
+# ax.xaxis.set_major_formatter(dates.DateFormatter('%m-%d'))
+#
+# plt.tight_layout()
+# plt.show()
+
+df_recent = df[-60:]
+print()
+increment_mean = df_recent["increment"].mean()
+ax.plot_date(df_recent.index.to_pydatetime(), df_recent["increment"], 'o-')
+# ax.plot_date(df["time"], df["total"], 'v-')
+ax.xaxis.set_minor_locator(dates.MinuteLocator(interval=10))
+ax.xaxis.set_minor_formatter(dates.DateFormatter('\n%H:%M'))
+ax.xaxis.grid(True, which="minor")
+ax.yaxis.grid()
+ax.xaxis.set_major_locator(dates.DayLocator())
+ax.xaxis.set_major_formatter(dates.DateFormatter('%m-%d'))
+# axvline 竖直  axhline 横向
+ax.axhline(35, ls="--", color="blue")
+ax.axhline(increment_mean, ls="--", color="red")
+
+ax.annotate('local max', xy=(20, 20), xytext=(5, 5),
+            arrowprops=dict(facecolor='black', shrink=1),
+            )
+
+plt.tight_layout()
+plt.show()
---
+++ KnowledgeMapping/MatplotlibExp/1-code/printResult.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import smtplib
+from email.mime.text import MIMEText
+import time
+
+
+def send_email(con="你好！"):
+    _user = "15999543812@163.com"
+    _pwd = "nickliqian2017"
+    _to = "419845955@qq.com"
+
+    # 使用MIMEText构造符合smtp协议的header及body
+    msg = MIMEText(con)
+    subject = "以下是今天的报表，请查收！--" + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    # # msgRoot['From'] = Header("菜鸟教程", 'utf-8')
+    msg["Subject"] = subject
+    msg["From"] = _user
+    msg["To"] = _to
+
+    s = smtplib.SMTP("smtp.163.com", timeout=30)  # 连接smtp邮件服务器,端口默认是25
+    s.login(_user, _pwd)  # 登陆服务器
+    s.sendmail(_user, _to, msg.as_string())  # 发送邮件
+    s.close()
+
+
+def deal_data():
+    # time作为列内容
+    df = pd.read_csv("./count.csv", names=["time", "total"], dtype={"total": np.int64}, parse_dates=[0], encoding="utf-8")
+
+    df60 = df[-60:]
+    increment = df60['total'].diff()
+    df60["increment"] = increment
+    # print(df)
+
+    df.plot(figsize=(12, 8))
+
+    # plt.figure('data & model')
+    # plt.plot(df["time"], df["total"], 'k', lw=3)
+    # # plt.savefig('result.png')
+    # plt.show()
+
+
+def check():
+    df = deal_data()
+    while True:
+        try:
+            send_email(con=df)
+            break
+        except smtplib.SMTPDataError as e:
+            if isinstance(e, smtplib.SMTPSenderRefused):
+                pass
+            elif isinstance(e, smtplib.SMTPDataError):
+                pass
+            else:
+                raise e
+        time.sleep(20)
+
+
+def main():
+    s = deal_data()
+    print(s)
+
+
+
+if __name__ == '__main__':
+    main()
