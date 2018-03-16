d9bdbb27c963c6da694914a1225348a7b6c4ae7d
Nick <nickliqian@outlook.com>
2018-03-16 22:15:13 +0800

    add udp code

---
+++ ReadBook/FanCH_PythonSpider/2-Code/3-udp编程/2-udpClient.py
@@ -0,0 +1,9 @@
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for data in [b'Hello', b'World']:
+    s.sendto(data, ('127.0.0.1', 9999))
+    print(s.recv(1024).decode('utf-8'))
+s.close()
