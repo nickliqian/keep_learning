80ff74fc004583bc4ddaa2832e3477b97414b26c
nick windows <nickliqian@outlook.com>
2018-04-25 23:46:10 +0800

    add python-kafka api

---
+++ ToolsStudy/kafka-python-demo/setting.py
@@ -0,0 +1,8 @@
+kafka_setting = {
+    'sasl_plain_username': 'LTAINlu0JjjObIuZ',
+    'sasl_plain_password': 'nfYF5wWPiX',
+    'bootstrap_servers': ["kafka-cn-internet.aliyun.com:8080"],
+    'topic_name': 'alikafka--spider-common',
+    'consumer_id1': 'CID_alikafka_spider_data',
+    'consumer_id2': 'CID_alikafka_spider_back',
+}
