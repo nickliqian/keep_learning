391c3b968305a6a72f196050771c59f7dc5a7db6
nick windows <nickliqian@outlook.com>
2018-11-21 22:05:21 +0800

    update code

--- ToolsStudy/kafka-python-demo/setting.py
+++
@@ -1,8 +0,0 @@
-kafka_setting = {
-    'sasl_plain_username': 'LTAINlu0JjjObIuZ',
-    'sasl_plain_password': 'nfYF5wWPiX',
-    'bootstrap_servers': ["kafka-cn-internet.aliyun.com:8080"],
-    'topic_name': 'alikafka--spider-common',
-    'consumer_id1': 'CID_alikafka_spider_data',
-    'consumer_id2': 'CID_alikafka_spider_back',
-}
