be120ce2bb94a8e8395630218985f5e51ae087d9
liqian <419845955@qq.com>
2020-07-31 17:52:20 +0800

    优化bitmap算法代码注释

--- ReadBook/数据结构实际案例/2.py
+++ ReadBook/数据结构实际案例/2.py
@@ -14,22 +14,38 @@ Python实现bitmap
 
 class BitMap(object):
     def __init__(self, max_value):
-        self._size = int((max_value + 31 - 1) / 31)  # 向上取正  确定数组大小
-        self.array = [0 for i in range(self._size)]  # 初始化为0  每个0代表 一个八位二进制 0000 0000
+        """
+        使用多个整型元素来储存数据，每个元素4个字节（32位）
+        """
+        self._size = int((max_value + 31 - 1) / 31)  # 计算需要的字节数，字节数也是数组的大小
+        self.array = [0 for i in range(self._size)]  # 数组的元素都初始化为0，每个元素有32位
 
-    def get_element_index(self, num):  # 获取该数的数组下标
+    @staticmethod
+    def get_element_index(num):
+        """
+        获取该数即将储存的字节在数组中下标
+        """
         return num // 31
 
-    def get_bit_index(self, num):  # 获取该数所在数组的位下标
+    @staticmethod
+    def get_bit_index(num):
+        """
+        获取该数在元素中的位下标
+        """
         return num % 31
 
-    def set(self, num):  # 将该数所在的位置1
+    def set(self, num):
+        """
+        将该数存在对应的元素的对应位置
+        """
         element_index = self.get_element_index(num)
         bit_index = self.get_bit_index(num)
-        self.array[element_index] = self.array[element_index] | (1 << bit_index)  # 当前8位写数操作
-        # print(element_index, bit_index, 1 << bit_index, self.array[element_index])
+        self.array[element_index] = self.array[element_index] | (1 << bit_index)
 
-    def find(self, num):  # 查找该数是否存在
+    def find(self, num):
+        """
+        查找该数是否存在与bitmap中
+        """
         element_index = self.get_element_index(num)
         bit_index = self.get_bit_index(num)
         if self.array[element_index] & (1 << bit_index):  # 1&1才是1，否则0；如果为1 ，说明数值存在
@@ -37,7 +53,10 @@ class BitMap(object):
         return False
 
     def count_one(self):
-        count = 0  # 用来计数
+        """
+        统计bitmap中数据的个数
+        """
+        count = 0
         for n in self.array:
             while n > 0:
                 n = n & (n - 1)
@@ -45,21 +64,22 @@ class BitMap(object):
         return count
 
 
-if __name__ == '__main__':
-
-    array_list = [1, 5, 45, 2, 78, 35, 67, 90, 879, 0, 340, 123, 46, 1, 5]
-    results = []
+def main():
+    array_list = [30, 32, 5, 45, 2, 78, 35, 67, 90, 879, 0, 340, 123, 46, 1, 5]
+    # 根据最大数的值创建BitMap对象
     bitmap = BitMap(max_value=max(array_list))
+
+    # 将数据逐个存入bitmap
     for num in array_list:
         bitmap.set(num)
 
+    # 查询数据是否存在于bitmap中
+    results = []
     for i in range(max(array_list) + 1):
         if bitmap.find(i):
             results.append(i)
-
-    print(array_list)
     print(results)
 
-    print(bitmap.count_one())
 
-    print(bitmap.find(3))
+if __name__ == '__main__':
+    main()
